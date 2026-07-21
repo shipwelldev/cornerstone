77879fb3a2f5df98c50e53f18dcb18b2f434380d
Ryan Bilesky <1036407+ryanshipswell@users.noreply.github.com>
2026-07-21 14:53:16 -0700

    adopt appp service provider rules

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,11 +4,34 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void {}
 
-    public function boot(): void {}
+    public function boot(): void
+    {
+        Date::useClass(dateClass: CarbonImmutable::class);
+
+        Model::shouldBeStrict();
+
+        Password::defaults(
+            static fn (): Password => app()->isProduction()
+                ? Password::min(12)->uncompromised()
+                : Password::min(8),
+        );
+
+        DB::prohibitDestructiveCommands(prohibit: app()->isProduction());
+
+        if (app()->isProduction()) {
+            URL::forceScheme(scheme: 'https');
+        }
+    }
 }
